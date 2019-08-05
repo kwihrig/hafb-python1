fe791e89fc93444ff614cb9221d63fc3d4ab0db2
kwihrig <Ziongithub1!>
2019-08-05 13:12:00 -0600

    Lists

--- List.py
+++ List.py
@@ -12,3 +12,8 @@ a = ["apple", "orange", "pear"]
 # Access by index notation
 print (a, a[0])
 print (a, a[1])
+# Replace an element
+a[0] = "tomatoes"
+print (a, a[1])
+a[1] = "3.14"
+print (a, a[1])
