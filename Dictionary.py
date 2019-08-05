a00eca7b166fe437c30f70e0acd23255d634cbc6
kwihrig <Ziongithub1!>
2019-08-05 13:58:24 -0600

    dictionaries

--- Dictionary.py
+++ Dictionary.py
@@ -12,3 +12,20 @@ d = {'alice': "801-123-45-8988",
 print (d, type(d))
 # Access one element
 print (d['pedro'])
+
+# Add members to the dictionary, of names -> grades
+roster={}      #Empty dictionary
+
+# Ask user to enter 3 names, and add them to the list
+total = 0
+while total < 3:
+    # Get key value
+    name = input("enter a name\n")
+    #Get value associated to key
+    score = input("enter score\n")
+    # Add element to dictionary.
+    # Note: if key value exists, it will update the value otherwise it will be added to the dict.
+    roster[name] = score
+    total +=1
+# display list
+print(roster)
