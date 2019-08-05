2eb8a6f50e26fdbd3560f353238b010542768d25
kwihrig <Ziongithub1!>
2019-08-05 11:13:56 -0600

    while loop update

--- while_loop.py
+++ while_loop.py
@@ -8,3 +8,11 @@ while counter != 0:
     print(counter)
     #Augmented Operator
     counter -= 1
+
+counter = 5
+while counter:
+        print(counter)
+        # Augmented Operator
+        counter -= 1
+
+print("Outside while loop")
