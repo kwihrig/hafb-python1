f2d5b817753d001750f4c86d04a1d5935ede6b07
kwihrig <Ziongithub1!>
2019-08-05 13:32:30 -0600

    dictionaries

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+"""
+Learn dictionaries
+Dictionaries map keys to values
+In some languages dictionaries are known as associative arrays or hashes, maps
+
+Create them using {} containing a key-value pair.
+Retrieve them by {key_value}
+"""
+d = {'alice': "801-123-45-8988",
+     'pedro': '956-445-78-8966',
+     'john': '651-321-66-4477'}
+print (d, type(d))
