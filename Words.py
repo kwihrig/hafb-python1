0a4f532e3d01d57b9e5fc91d927ed2aa06296647
kwihrig <Ziongithub1!>
2019-08-05 14:14:13 -0600

    dictionaries

---
+++ Words.py
@@ -0,0 +1,11 @@
+"""
+Get a file from the web:
+http://icarus.cs.weber.edu/~hvalle/hafb/words.txt
+
+Task 1: count number of words in document
+"""
+from urllib.request import urlopem
+file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
+
+
+
