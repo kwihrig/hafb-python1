d04ca1547845b5830549c04b0c4871d811c5f6d5
kwihrig <Ziongithub1!>
2019-08-05 11:32:23 -0600

    Strings & Collections

--- strings.py
+++ strings.py
@@ -6,3 +6,24 @@ A string is an immutable sequence of Unicode code-points
 print('This is a string')
 print("This is a string too")
 
+# Concatenation and Adjacent Strings
+s1 = "First"
+s2 = "Second"
+print (s1 + s2)
+
+# Multi-line Strings and newlines
+s3 = """This is 
+a multi-line string"""
+print(s3)
+s4 = "This is\na multi-line\nstring with new line"
+print(s4)
+# Support for backslash
+s5 = "A\\in a string"
+print(s5)
+
+
+
+
+
+
+
