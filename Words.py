20a1e659d65140d3f696bf3e43af88e75f304df0
kwihrig <Ziongithub1!>
2019-08-05 14:43:15 -0600

    words counting

--- Words.py
+++ Words.py
@@ -8,16 +8,18 @@ from urllib.request import urlopen
 file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
 
 count = 0
+data = {}           # Empty dictionary
 with urlopen(file) as story:
     for line in story:
        words = line.decode('utf-8').split() #split with space as default
 #       print(words)
        for word in words:
            if word in data:
-               data[word] +=1
+               data[word] += 1
            else:
-               data[word] +=1
-
+               data[word] = 1
            count += 1
 print ("Total number of words ", count)
+print ("Total data ", data)
+
 
