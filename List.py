ddac046a8db1faf9fd94185585fdc15bc4316f7a
kwihrig <Ziongithub1!>
2019-08-05 13:10:14 -0600

    Lists

--- List.py
+++ List.py
@@ -7,3 +7,8 @@ You can update and append elements to it
 # use brackets to define lists
 l=[1, 2, 3]
 print ("List ", l, type (l))
+# A list of objects (any type)
+a = ["apple", "orange", "pear"]
+# Access by index notation
+print (a, a[0])
+print (a, a[1])
