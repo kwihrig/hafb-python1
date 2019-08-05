4c3aea36db0c7167df0e26570c1e2b89403c84a0
kwihrig <Ziongithub1!>
2019-08-05 12:55:05 -0600

    bytes

---
+++ bytes.py
@@ -0,0 +1,11 @@
+"""
+Learn about bytes
+Bytes are similar to str type, but they are a swquence of
+bytes instead of Unicode code points.
+
+Use for raw binary data, fixed width records, single-byte encoding: ASCII
+
+Use the byte() constructor
+"""
+d= b'data'
+print (d, type(d))
