a6429890e94652c2f99918fa51c609b5b1356b7f
kwihrig <53785891+kwihrig@users.noreply.github.com>
2019-08-06 10:32:53 -0600

    take a break video

--- file_rename.py
+++ file_rename.py
@@ -11,9 +11,17 @@ def rename_files():
     """
     folder_dir = r"C:\Users\kevinihrig\Desktop\hafb\prankOrig"
     files = os.listdir(folder_dir)
+    saved_path = os.getcwd()   # save current directory
+    # go to the file directory
+    os.chdir(folder_dir)
     for file in files:
-        new_file = file.lstrip('0123456789')
+        # remove digits from file name
+        new_file = file.lstrip('2345678910')
         print("old name", file, "     new name", new_file)
+        # Rename file to new name
+        os.rename(file, new_file)
+# go to the home directory
+    os.chdir(saved_path)
 
 
 
