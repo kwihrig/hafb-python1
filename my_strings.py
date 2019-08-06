421f29c70350623560a10f7e22f002eb0213c94c
kwihrig <kwihrig@yahoo.com>
2019-08-06 15:36:54 -0600

    reference functions

---
+++ my_strings.py
@@ -0,0 +1,22 @@
+"""
+File Info Here
+"""
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+    s1 = "This is super cool"
+    print("Size of s1", len(s1))
+    # concatenation "+")
+    s2 = "Weber" + "State" + "University"
+    print(s2)
+    # if you need to join large strings, use the join () method
+    # instead of + operator
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
