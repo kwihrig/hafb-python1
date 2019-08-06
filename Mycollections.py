07f8e124a27d56c6039b66dba689ea64e2c93052
kwihrig <kwihrig@yahoo.com>
2019-08-06 15:28:16 -0600

    reference functions

--- Mycollections.py
+++ Mycollections.py
@@ -25,6 +25,13 @@ def do_tuples():
     # Parentheses are optional
     t2 = 1, 2, 3, 5
     print(t2, type(t2))
+    #tuple constructor: tuple()
+    t_from_l = tuple([3, 77, 11])
+    print (t_from_l, type(t_from_l))
+    #test for membership
+    print(5 in (3, 6, 8, 5, 12))
+    print(5 not in (3, 6, 8, 5, 12))
+
 
 def min_max(items):
     """
@@ -49,24 +56,24 @@ def main():
     test function
     :return: none
     """
-    #do_tuples()
-    output = min_max ([56,76,11,12,90])
-
-    print("min", output[0])
-    print("max", output[1])
-    #tuple unpacking
-    lower, upper = min_max([56, 76, 11, 12, 90])
-    print("min", lower)
-    print("max", upper)
-    #Swap values
-    a = "jelly"
-    b = "bean"
-    print(a,b)
-    # call you function
-    a, b = swap(a,b)
-    print(a,b)
-    a, b = b, a
-    print(a,b)
+    do_tuples()
+    # output = min_max ([56,76,11,12,90])
+    #
+    # print("min", output[0])
+    # print("max", output[1])
+    # #tuple unpacking
+    # lower, upper = min_max([56, 76, 11, 12, 90])
+    # print("min", lower)
+    # print("max", upper)
+    # #Swap values
+    # a = "jelly"
+    # b = "bean"
+    # print(a,b)
+    # # call you function
+    # a, b = swap(a,b)
+    # print(a,b)
+    # a, b = b, a
+    # print(a,b)
 
 
 
