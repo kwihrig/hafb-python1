3ec44b1235962e35051a3cd78856008e531096af
kwihrig <53785891+kwihrig@users.noreply.github.com>
2019-08-06 09:38:16 -0600

    take a break video

--- take_break.py
+++ take_break.py
@@ -9,10 +9,10 @@ def main():
     test function
     :return: none
     """
-    video_address = "https://youtu.be/aJg4OJxp-co"
+    video_address = "https://youtu.be/mXAs7Nva7kc"
     webbrowser.open(video_address)
 
 
-    if __name__== '__main__':
+if __name__== '__main__':
         main()
         exit(0)
