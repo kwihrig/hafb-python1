221cfff59bbc4580f7ac5d813cf7a51c2b347e24
kwihrig <53785891+kwihrig@users.noreply.github.com>
2019-08-06 09:19:11 -0600

    Add files via upload

--- wordfn.py
+++ wordfn.py
@@ -1,29 +1,46 @@
 """
 To count words in a file
-ALso counts occurrences of individual words
+Also counts occurrences of individual words
 """
 from urllib.request import urlopen
 
-file = "filename"
-
-def fetch_words()
+def fetch_words(filename):
     """
+    Count words in url file
+    :param filename: url to file
     Fetch the words from a file on the web
-    :return: 
+    :return: a list with the items
     """
     count = 0
-    data = {}           # Empty dictionary
-    with urlopen(file) as story: # story is the variable for the text in the file
+    data = []           # Empty list
+    with urlopen(filename) as story: # story is the variable for the text in the file
         for line in story:
            words = line.decode('utf-8').split() #split line into words using space as default separator
 #           print(words)
            for word in words:
-               if word in data:
-                   data[word] += 1
-               else:
-                   data[word] = 1
-               count += 1
-print ("Total number of words ", count)
+               data.append(word)
+    return data
 
-print ("Total data ", data)
+def print_items(items):
+    """
+    Print elements of the collection
+    :param items: a collection of objects
+    :return: nothing
+    """
+    for item in items:
+        print(item)
 
+
+
+def main():
+    """
+    test function for words library
+    :return: nothing
+    """
+    file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
+    words = fetch_words(file)
+    print_items(words)
+
+if __name__== '__main__':
+    main()
+    exit(0)
