f5169674edb20664f0b1897d6597197ebe573b73
kwihrig <53785891+kwihrig@users.noreply.github.com>
2019-08-06 09:33:43 -0600

    take a break video

---
+++ take_break.py
@@ -0,0 +1,18 @@
+"""
+Set alarm to play a video
+"""
+import webbrowser
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+    video_address = "https://youtu.be/aJg4OJxp-co"
+    webbrowser.open(video_address)
+
+
+    if __name__== '__main__':
+        main()
+        exit(0)
