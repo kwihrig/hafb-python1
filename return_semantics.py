df3478040dca2dbd407a50dffa46a7fb68915413
kwihrig <kwihrig@yahoo.com>
2019-08-06 13:07:37 -0600

    reference functions

--- return_semantics.py
+++ return_semantics.py
@@ -10,14 +10,31 @@ def egg(var):
     """
     return var
 
+def sum_two(num1, num2):
+    """
+    sum 2 input objects
+    :param num1: object 1
+    :param num2: object 2
+    :return: sum of objects
+    """
+    return num1 + num2
+
+
 
 def main():
     """
     test function
     :return: none
     """
-  pass
 
+    c = [6,10,20]
+    e = egg(c)
+    print (c is e)
+    #n1 = 3
+    #n2 = 9
+    n1 = "hot"
+    n2 = "dog"
+    print(n1," + ", n2, " = ", sum_two(n1, n2))
 
 
 if __name__== '__main__':
