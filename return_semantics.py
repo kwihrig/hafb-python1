b98b4dedaefa29b63b53fbce91dc9b2c56918e02
kwihrig <kwihrig@yahoo.com>
2019-08-06 12:58:12 -0600

    reference functions

--- return_semantics.py
+++ return_semantics.py
@@ -2,6 +2,14 @@
 File Info Here
 """
 
+def egg(var):
+    """
+    dummy function returns variable back to user
+    :param var: input object
+    :return: input object
+    """
+    return var
+
 
 def main():
     """
