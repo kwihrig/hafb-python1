c6e640b4b820e241e625614218c480ff4c2b71da
kwihrig <kwihrig@yahoo.com>
2019-08-06 15:38:56 -0600

    reference functions

--- my_strings.py
+++ my_strings.py
@@ -15,6 +15,9 @@ def main():
     print(s2)
     # if you need to join large strings, use the join () method
     # instead of + operator
+    teams = ["Real Madrid", "Barcelona", "Manchester United"]
+    record = ":".join(teams)
+    print (record)
 
 
 if __name__ == '__main__':
