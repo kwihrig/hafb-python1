b0b8ed6dcc7628dce54c4660dc89e7a8b5b56966
kwihrig <kwihrig@yahoo.com>
2019-08-06 14:48:57 -0600

    reference functions

---
+++ Mycollections.py
@@ -0,0 +1,28 @@
+"""
+File learn about collectoin: Tuples, Strings, range, List, Dictionaries, Sets
+"""
+
+
+def do_tuples():
+    """
+    practice tuples
+    :return: none
+    """
+    #Immutable sequence of arbitrary objects
+    # Use() to define a tuple
+    t=("Odgen", 1.99, 2)
+    print(t,type(t))
+
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+    do_tuples()
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
