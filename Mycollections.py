38da5b47a0dcb3872a21821489c48f76944a8c5f
kwihrig <kwihrig@yahoo.com>
2019-08-06 15:04:03 -0600

    reference functions

--- Mycollections.py
+++ Mycollections.py
@@ -26,12 +26,31 @@ def do_tuples():
     t2 = 1, 2, 3, 5
     print(t2, type(t2))
 
+def min_max(items):
+    """
+    return min adn max elements of a collection
+    :param items: collection
+    :return: min and max
+    """
+    return min(items), max (items)
+
+
 def main():
     """
     test function
     :return: none
     """
-    do_tuples()
+    #do_tuples()
+    output = min_max ([56,76,11,12,90])
+
+    print("min", output[0])
+    print("max", output[1])
+    #tuple unpacking
+    lower, upper = min_max([56, 76, 11, 12, 90])
+    print("min", lower)
+    print("max", upper)
+
+
 
 
 if __name__ == '__main__':
