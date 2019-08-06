8e04f6700ee08e8f20a47536c2ceeb83597a91ac
kwihrig <kwihrig@yahoo.com>
2019-08-06 14:20:43 -0600

    reference functions

--- scope.py
+++ scope.py
@@ -1,6 +1,26 @@
 """
-File Info Here
+Learn about scoping in Python
 """
+count = 0   # global cbject
+
+
+def show_count():
+    """
+    Display current count
+    :return: none
+    """
+    print(count)
+
+
+def set_count(num):
+    num=0
+    """
+    set global counter to input
+    :param num: input number
+    :return: none
+    """
+    global count
+    count = num
 
 
 def main():
@@ -8,9 +28,13 @@ def main():
     test function
     :return: none
     """
+    show_count()
+    set_count(9)
+    show_count()
+#    print(count)
 
 
-pass
+
 
 if __name__ == '__main__':
     main()
