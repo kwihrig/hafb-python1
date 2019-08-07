d8b175c8e54d1907078416d5d890c74257e37688
kevinihrig <kwihrig@yahoo.com>
2019-08-07 10:59:18 -0600

    file about range functions

---
+++ my_dictionaries.py
@@ -0,0 +1,39 @@
+"""
+Learn about dictionaries
+"""
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+    urls = {
+        "google":"www.google.com",
+        "yahoo":"www.yahoo.com",
+        "twitter":"twitter.com",
+        "wsu":"weber.edu"
+    }
+
+    print (urls.type(urls))
+    # access by key:[key]
+    print (urls["wsu"])
+    # build dict with dict() constructor
+    names_age = [('Alice', 32), ('Mario', 23), ('Hugo', 21)]
+    d = dict(names_age)
+    print(d)
+    # another method)
+    phonetic = dict(a='alpha', b='bravo', c='charlie', d='delta')
+    print (phonetic)
+    # make a copy
+    e = phonetic.copy()
+    print (e)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
