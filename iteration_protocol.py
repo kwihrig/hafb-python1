cf3f3c6f5533ee0cc32ee5bc96957dcd78e75f12
kevinihrig <kwihrig@yahoo.com>
2019-08-07 15:49:19 -0600

    file about iterator and next

--- iteration_protocol.py
+++ iteration_protocol.py
@@ -5,6 +5,13 @@ iter(): create an iterable object
 next(): fetch the next element in the iterable object
 """
 
+def first(iterable):
+    """
+    return the next member of the list if avail
+    :param iterable: iterable object
+    :return: Next member or
+    :except: ValueError for StopIteration
+    """
 
 def main():
     """
@@ -15,9 +22,7 @@ def main():
     iterator = iter(iterable) #give me an iterator
     print (type(iterator), iterator)
     print (next(iterator))
-    print (next(iterator))
-    print (next(iterator))
-    print (next(iterator))
+
 
 
 if __name__ == '__main__':
