d836ec38eeea9ad8a8e1950371c9f11cfdbdea38
kevinihrig <kwihrig@yahoo.com>
2019-08-07 12:54:17 -0600

    file about range functions

--- random_rolls.py
+++ random_rolls.py
@@ -7,20 +7,30 @@ def roll_die(num):
     """
     Random roll of adie
     :param num:number of rolls
-    :return: a
+    :return: a list of frequencies
+    Index 0 maps to 1
+    Index 1 maps to 2
+    etc
     """
+    freq = [0] * 6         # list initial values to 0
+    for roll in range(100):
+#        freq.append()
+        print(random.randint(1, 6))
+        print(freq)
+
+
 
 def main():
     """
     test function
     :return: none
     """
-    for x in range(6000):
-        print(random.randint(1, 6))
+    num = int(input("How many times you need to roll"))
+    roll_die(num)
 
 
 #    for roll in range(10):
-#       print (random.randrange(1,6))
+#       print (random.randrange(1,7))
 
 
 if __name__ == '__main__':
