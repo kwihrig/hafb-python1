777a99b6c589e06a2faa6425152888a27ec8fe9d
kevinihrig <kwihrig@yahoo.com>
2019-08-07 14:49:08 -0600

    file about list comprehensions

---
+++ list_comprehensions.py
@@ -0,0 +1,18 @@
+"""
+Learn list comprehensions
+Readable, expressive, and effective.
+"""
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+    words = "Today I am very happy to learn about list comprehensions".split()
+    print(words)
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
