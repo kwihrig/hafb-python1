074ab04b6780e571bd71b59491d7e6a17854c37a
kevinihrig <kwihrig@yahoo.com>
2019-08-07 15:36:15 -0600

    file about list comprehensions

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -2,7 +2,7 @@
 Learn list comprehensions
 Readable, expressive, and effective.
 """
-from math import factorial
+from math import factorial, sqrt
 from pprint import pprint as pp
 
 
@@ -40,7 +40,30 @@ def main():
     nba_teams = {'jazz':'slc', 'warriors':'oakland', 'lakers':'LA', 'clippers':'LA'}
     pp(nba_teams)
     teams_nba = {city:team for team, city in nba_teams.items()}
-    pp(teams_nba)
+    pp(teams_nba) # in this example, we lose the clippers because they are from the same city?
+
+    # Filter Predicates
+    primes = [x for x in range(1001) if is_prime(x)]
+    print(len(primes), primes)
+def is_prime(num):
+    """
+    finds primes
+    :param num: number to test
+    :return: true for prime numbers
+    false for non-primes
+    """
+
+    if num < 2:
+        return False
+    for i in range(2, int(sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+
+
 
 if __name__ == '__main__':
     main()
