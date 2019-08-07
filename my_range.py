b66aa6c2dfe7ccbbc1833b4c823b58138717e3d4
kevinihrig <kwihrig@yahoo.com>
2019-08-07 08:46:58 -0600

    file about range functions

--- my_range.py
+++ my_range.py
@@ -8,8 +8,37 @@ def main():
     test function
     :return: none
     """
+    
+    #Default
+    for i in range(5):
+        print(i)
+        
+# Set the initial value
+    print("Now setting initial value ")
+    for i in range(5, 10):
+        print(i)
+        
+# Create a list from range
+    l = list(range(5, 10))
+    print(l, type (l))
+    l2 = list(range(5,10)) + list (range(30,40)):
+    print(l2, type(l2))
+# Step arguement: (begin, stop step)
+    l3 = list(range(0,20,2))
+    print(l3, type(l3))
+    #iterationover list using index notation
+    s=[0,2,4,6]
+    for i in range(len(s))
+            print(s[i])
+# better way
+    for v in s:
+        print(v)
 
-    print
+# enumerat (): retnr iterable series
+    t = [6, 789, 128, 98, 3, 22]
+    for p in enumerate(t):
+        print (p)
+
 
 
 if __name__ == '__main__':
