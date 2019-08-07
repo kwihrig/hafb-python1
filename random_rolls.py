147cbe5db5e8dc8341ad296c1d1f81bb7e327c47
kevinihrig <kwihrig@yahoo.com>
2019-08-07 12:47:48 -0600

    file about range functions

---
+++ random_rolls.py
@@ -0,0 +1,28 @@
+"""
+Simulate 6000 rolls of a die (1-6)
+"""
+import random
+
+def roll_die(num):
+    """
+    Random roll of adie
+    :param num:number of rolls
+    :return: a
+    """
+
+def main():
+    """
+    test function
+    :return: none
+    """
+    for x in range(6000):
+        print(random.randint(1, 6))
+
+
+#    for roll in range(10):
+#       print (random.randrange(1,6))
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
