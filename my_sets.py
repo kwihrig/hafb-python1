549e32dd2c84ec3c80fff01a37c01ece036fea2d
kevinihrig <kwihrig@yahoo.com>
2019-08-07 13:17:26 -0600

    file about sets

--- my_sets.py
+++ my_sets.py
@@ -13,6 +13,12 @@ def main():
     """
     p={6, 78, 21, 45}
     print(p, type(p))
+    data = [1, 3, 5, 2, 88, 3, 1]
+    print(data, type(data))
+    # eliminated dups
+    sdata = set(data)
+    print(sdata, type(sdata))
+
 
 
 
