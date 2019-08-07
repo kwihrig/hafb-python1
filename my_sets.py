63cd084e3d0cc0d9411c9d55a55ce53421ecde1a
kevinihrig <kwihrig@yahoo.com>
2019-08-07 13:15:56 -0600

    file about sets

---
+++ my_sets.py
@@ -0,0 +1,21 @@
+"""
+Learn about sets
+an unordered collection of unique, immutable objects
+Define it using {} but no key needed
+You can use the set constructor to make one
+"""
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+    p={6, 78, 21, 45}
+    print(p, type(p))
+
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
