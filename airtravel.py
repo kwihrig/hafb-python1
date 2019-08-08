b957ca1686b32574a5d5b6692b658ff76ca22ded
kevinihrig <kwihrig@yahoo.com>
2019-08-08 13:14:33 -0600

    aircraft files

--- airtravel.py
+++ airtravel.py
@@ -7,8 +7,32 @@ class Flight:
     A flight with a particular passenger aircraft
     """
     def __init__(self, number):
+        """
+        Initializes flight number
+        :param number: flight number
+        :raise: ValueError (for invalid format)
+        """
         # implementation details begin with '_'
-        self._number = number # if a variable begins with one underscore, it is a private variable
+        # validate flight number
+        # 5 chars long, AADDD A-> ALPHA, D-> Digit
+        if len(number) != 5:
+            raise ValueError("Invalid flight number length {} "
+                             .format(number))
+        if not number[:2].isalpha():
+            raise ValueError("No airline code {}"
+                             .format(number))
+        if not number[:2].isupper():
+            raise ValueError("Not uppercase {}"
+                             .format(number))
+        if not number[2:].isdigit():
+            raise ValueError("Invalid route code {}"
+                             .format(number))
+
+        # for char in number:
+        #     char = number.decode('utf-8').split()  # split with space as default
+        #       print(words)
+
+        self._number = number  # if a variable begins with one underscore, it is a private variable
 
     def number(self):
         """
--- use_airtravel.py
+++ use_airtravel.py
@@ -10,8 +10,14 @@ def main():
     """
     f = Flight("SN066")
     print(f.number())
-    f2 = Flight("SN013")
-    print(f2, f2.number())
+    # f2 = Flight("S13")
+    # print(f2, f2.number())
+
+    f3 = Flight("ab345")
+    print(f3, f3.number())
+    # f2 = Flight("SN013")
+    # print(f2, f2.number())
+
     #Could use: Flight.number(f)
 
 if __name__ == '__main__':
