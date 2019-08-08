6121e6e0649a7462e94c5bc523104d5a70729291
kevinihrig <kwihrig@yahoo.com>
2019-08-08 10:24:59 -0600

    Updated with day 3 summary & some bio on dr. Valle.

---
+++ my_iterations.py
@@ -0,0 +1,24 @@
+"""
+When working with iterators, generators, etc,
+Look at the documentation for the itertools module
+"""
+from itertools import islice, count
+from list_comprehensions import is_prime
+
+
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+    thousand_primes = islice((x for x in count() if is_prime(x)), 1000)
+    print(thousand_primes, type(thousand_primes))
+    print("list of first thousand prime numbers", list(thousand_primes))
+    print("list of first thousand prime numbers", sum(thousand_primes))
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
