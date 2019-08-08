1a75db87f381284febd03ce9d76aa89e6027c79c
kevinihrig <kwihrig@yahoo.com>
2019-08-08 12:38:23 -0600

    aircraft files

---
+++ airtravel.py
@@ -0,0 +1,23 @@
+"""
+A flight class. Model for aircraft flights
+"""
+
+class Flight:
+    """
+    A flight with a particular passenger aircraft
+    """
+    pass
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+
+    print("Done")
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
---
+++ use_airtravel.py
@@ -0,0 +1,20 @@
+"""
+Use flight class
+"""
+
+from airtravel import Flight
+
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+    f = Flight()
+    print(f, type(f))
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
