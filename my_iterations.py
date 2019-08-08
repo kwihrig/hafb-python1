ef7727523ac089cdaa99005c810d0fa32c95fdde
kevinihrig <kwihrig@yahoo.com>
2019-08-08 10:43:20 -0600

    Updated with day 3 summary & some bio on dr. Valle.

--- my_iterations.py
+++ my_iterations.py
@@ -19,6 +19,11 @@ def main():
     # Note: if you need to use the object again, you need to regenerate it, or you get a 0 value.
     thousand_primes = islice((x for x in count() if is_prime(x)), 1000)
     print("Sum of first thousand prime numbers", sum(thousand_primes))
+# other built-ins used with itertools: any ("or"), all ("and")
+    print(any([False, False, True]))
+    print(all([False, False, True]))
+    print("Are there any primes between 1328-1361?", any(is_prime(x) for x in (range(1328, 1362))))
+
 
 
 if __name__ == '__main__':
