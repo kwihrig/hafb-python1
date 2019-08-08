bb74b24987b2032f7cd686aee9a30b3a216ad5d4
kevinihrig <kwihrig@yahoo.com>
2019-08-08 08:36:16 -0600

    Updated with day 3 summary & some bio on dr. Valle.

---
+++ generator_functions.py
@@ -0,0 +1,27 @@
+"""
+Learn about exceptions
+when an exception is found, normal flow of program is interrupted
+
+"""
+
+
+
+
+def main():
+    """
+    test function
+    :return: none
+    """
+
+
+
+    print("Done")
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
